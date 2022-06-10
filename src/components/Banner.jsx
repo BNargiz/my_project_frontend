ffe7aaa21bc082b53a04500333498ff89a061474
BNargiz <nargiz493@gmail.com>
2022-06-10 12:59:35 +0400

    changed text on homepage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,12 @@ const Banner = () => {
   return (
     <div className="homepage-header">
       <div className="banner-overlay">
-        <h1 className="homepage-title">
-          Hi parents, On this <br /> website you will find fun
-        </h1>
+        <h1 className="homepage-title">Enjoying time with your child!</h1>
         <p className="homepage-description">
-          and interesting activities for your kids - from play dates to drawing
-          classes and museum tours. Any member of our community can register and
-          post activities so we can have our young ones entertained.
+          Hi parents, on this website you will find fun and interesting
+          activities for your kids - from play dates to drawing classes and
+          museum tours. Any member of our community can register and post
+          activities so we can have our young ones entertained.
         </p>
       </div>
     </div>
